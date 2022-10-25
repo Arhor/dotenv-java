6c2c92763f36c8b78547cd4ab9bbe2099908ab94
Maskim Buryshynets <Maksim_Buryshynets@epam.com>
2022-10-25 16:42:09 +0300

    tests moved to different nested classes;

--- src/test/java/com/github/arhor/dotenv/DotenvImplTest.java
+++ src/test/java/com/github/arhor/dotenv/DotenvImplTest.java
@@ -1,57 +1,72 @@
 package com.github.arhor.dotenv;
 
-import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 class DotenvImplTest {
 
-    @Test
-    void should_not_fail_1() {
-        // given
-        final Dotenv dotenv = Dotenv.configure().load();
-        final String propertyName = "test_key";
-        final String propertyValue = "test_value";
+    @Nested
+    @DisplayName("Dotenv.get(String)")
+    class DotenvGetTests {
+        @Test
+        void should_not_fail_1() {
+            // given
+            final Dotenv dotenv = Dotenv.configure().load();
+            final String propertyName = "test_key";
+            final String propertyValue = "test_value";
 
-        // when
-        final String result = dotenv.get(propertyName);
+            // when
+            final String result = dotenv.get(propertyName);
 
-        // then
-        assertThat(result)
-            .isNotNull()
-            .isEqualTo(propertyValue);
+            // then
+            assertThat(result)
+                .isNotNull()
+                .isEqualTo(propertyValue);
+        }
+
+        @Test
+        void should_not_fail_2() {
+            // given
+            final Dotenv dotenv = Dotenv.configure().location("./directory").load();
+            final String propertyName = "test_key";
+            final String propertyValue = "test_value";
+
+            // when
+            final String result = dotenv.get(propertyName);
+
+            // then
+            assertThat(result)
+                .isNotNull()
+                .isEqualTo(propertyValue);
+        }
+
+        @Test
+        void should_not_fail_3() {
+            // given
+            final Dotenv dotenv = Dotenv.configure().load();
+            final String propertyName = "c";
+
+            // when
+            final var result = catchThrowable(() -> dotenv.get(propertyName));
+
+            // then
+            assertThat(result)
+                .isInstanceOf(CyclicReferenceException.class)
+                .hasMessageContaining("a -> b -> c -> a");
+        }
     }
 
-    @Test
-    void should_not_fail_2() {
-        // given
-        final Dotenv dotenv = Dotenv.configure().location("./directory").load();
-        final String propertyName = "test_key";
-        final String propertyValue = "test_value";
-
-        // when
-        final String result = dotenv.get(propertyName);
-
-        // then
-        assertThat(result)
-            .isNotNull()
-            .isEqualTo(propertyValue);
+    @Nested
+    @DisplayName("Dotenv.get(String, String)")
+    class DotenvGetWithDefaultTests {
     }
 
-    @Test
-    void should_not_fail_3() throws Throwable {
-        // given
-        final Dotenv dotenv = Dotenv.configure().load();
-        final String propertyName = "c";
-
-        // when
-        final ThrowingCallable action = () -> dotenv.get(propertyName);
-
-        // then
-        assertThatExceptionOfType(CyclicReferenceException.class)
-            .isThrownBy(action)
-            .withMessageContaining("a -> b -> c -> a");
+    @Nested
+    @DisplayName("Dotenv.getRequired(String)")
+    class DotenvGetRequiredTests {
     }
 }
